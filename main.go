c6ae35fd42adff3c0cbd0e31d02ac1e01c7474e6
Patryk Perduta <patryk@perduta.net>
2022-03-01 09:58:33 +0100

    Rewrite main.go

--- main.go
+++ main.go
@@ -3,51 +3,55 @@ package main
 import (
 	"aoc2021/solutions"
 	"bufio"
-	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
-func main() {
-	SOLUTION_FNS := map[int][]func([]string) int{
-		1: {solutions.Day1Part1, solutions.Day1Part2},
-		2: {solutions.Day2Part1, solutions.Day2Part2},
-		3: {solutions.Day3Part1, solutions.Day3Part2},
-		4: {solutions.Day4Part1, solutions.Day4Part2},
-		5: {solutions.Day5Part1, solutions.Day5Part2},
-		6: {solutions.Day6Part1, solutions.Day6Part2},
-		7: {solutions.Day7Part1, solutions.Day7Part2},
-		8: {solutions.Day8Part1, solutions.Day8Part2},
-		9: {solutions.Day9Part1, solutions.Day9Part2},
-		10: {solutions.Day10Part1, solutions.Day10Part2},
-		11: {solutions.Day11Part1, solutions.Day11Part2},
-	}
+var SOLUTIONS = [][]func([]string) int{
+	{solutions.Day1Part1, solutions.Day1Part2},
+	{solutions.Day2Part1, solutions.Day2Part2},
+	{solutions.Day3Part1, solutions.Day3Part2},
+	{solutions.Day4Part1, solutions.Day4Part2},
+	{solutions.Day5Part1, solutions.Day5Part2},
+	{solutions.Day6Part1, solutions.Day6Part2},
+	{solutions.Day7Part1, solutions.Day7Part2},
+	{solutions.Day8Part1, solutions.Day8Part2},
+	{solutions.Day9Part1, solutions.Day9Part2},
+	{solutions.Day10Part1, solutions.Day10Part2},
+	{solutions.Day11Part1, solutions.Day11Part2},
+}
 
-	files, err := ioutil.ReadDir("inputs/")
+func parseInput(filename string) ([]string, error) {
+	file, err := os.Open("inputs/" + filename)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	if len(files) == 0 {
-		log.Fatal("No inputs found. Please add inputs of your puzzles to the inputs/ directory.")
+	defer file.Close()
+	inputs := []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputs = append(inputs, scanner.Text())
 	}
-	for _, fileInfo := range files {
-		n, err := strconv.Atoi(fileInfo.Name())
+	return inputs, nil
+}
+
+func main() {
+	totalTime := time.Now()
+	for i, fns := range SOLUTIONS {
+		day := i + 1
+		inputs, err := parseInput(strconv.Itoa(day))
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Could not parse inputs for day %v, skipping...", day)
+			continue
 		}
-		file, err := os.Open("inputs/" + fileInfo.Name())
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		inputs := []string{}
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			inputs = append(inputs, scanner.Text())
-		}
-		for _, solutionFn := range SOLUTION_FNS[n] {
-			log.Println("Day", n, "answer", solutionFn(inputs))
+		for j, fn := range fns {
+			now := time.Now()
+			result := fn(inputs)
+			elapsed := time.Since(now)
+			log.Printf("Day %2v, part %v answer: %16v (took %v)", day, j+1, result, elapsed)
 		}
 	}
+	log.Printf("Calculating all the answers took %v", time.Since(totalTime))
 }
